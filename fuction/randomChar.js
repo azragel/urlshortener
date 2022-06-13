db94e69a627f8743fa267dff85f4d4daef1a6825
azragel <azragel@gmail.com>
2022-06-13 20:56:17 +0800

    feat: fix randomChars bug

--- fuction/randomChar.js
+++ fuction/randomChar.js
@@ -1,8 +1,8 @@
 function randomChar (digit) {
   const charPool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   let chars = ''
-  for (i = 0; i < digit; i++) {
-    const index = Math.floor(Math.random() * charPool.length - 1)
+  for (let i = 0; i < digit; i++) {
+    const index = Math.floor(Math.random() * charPool.length)
     chars += charPool[index]
   }
   return chars
